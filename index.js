056d4770611b34ee24f8830110bcae7533d3108f
Abhishek KumarTamrakar <abhishektamrakar013@gmail.com>
2024-09-11 19:10:43 +0530

    added Indexjs and Integrated all routes,models and controllers

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const authRoutes = require("./routes/authRoutes");
+const crudTransactionRoutes = require("./routes/crudTransactionRoutes");
+const crudSavingsRoutes = require("./routes/crudSavingRoutes");
+const curdCategoryRoutes = require("./routes/crudCategoryRoutes")
+const reportRoutes = require("./routes/reportRoutes")
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
+const PORT = 5000;
+
+
+const url_local='mongodb://127.0.0.1:27017/finance';
+mongoose
+  .connect(url_local)
+  .then((msg) => {
+    console.log("DB Connected to rest of the world");
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("DB Connection Failed");
+  });
+app.use(crudTransactionRoutes);
+app.use(authRoutes);
+app.use(curdCategoryRoutes);
+app.use(crudSavingsRoutes);
+app.use(reportRoutes)
+// 
+app.listen(PORT,()=>
+{
+    console.log("Server running on Port 5000");
+})
